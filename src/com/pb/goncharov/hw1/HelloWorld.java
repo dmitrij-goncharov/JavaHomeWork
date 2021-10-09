5985807459a6c361431f432760d4a14fcf3cc23b
dmitrij-goncharov <dmitrij.goncharov@privatbank.ua>
2021-10-09 21:17:56 +0300

    init commit

---
+++ src/com/pb/goncharov/hw1/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.pb.goncharov.hw1;
+
+import jdk.nashorn.internal.parser.JSONParser;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
